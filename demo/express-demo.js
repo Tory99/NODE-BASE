fcb7f40db3049a8b2f644f03f344e774b93e7faf
tory99 <hjw990826@gmail.com>
2025-02-19 14:51:19 +0900

    02.19

--- demo/express-demo.js
+++ demo/express-demo.js
@@ -1,5 +1,34 @@
 const express = require('express')
 const app = express()
+app.listen(3000)
+
+const game = [
+  { id : 1, name : "lol"},
+  { id : 2, name : "PUBG"},
+  { id : 3, name : "overwatch"},
+  { id : 4, name : "maplestory"},
+]
+
+app.get('/Search', (req, res) => {
+  res.json(game)
+})
+
+app.get('/Search/:id', (req,res) => {
+  let {id} = req.params
+  id = parseInt(id)
+  var findgame = game.find( f => f.id == id )
+
+  if(findgame){
+    // game.forEach((id) => {
+    //   if(game.id == id){
+    //     res.json(game)
+    //   }
+    res.json(findgame)
+
+  } else {
+    res.status(404).send("404, not find")
+  }
+})
 
 app.get('/', function (req, res) {
   res.send('main')
@@ -10,4 +39,3 @@ app.post('/test', function (req, res) {
 })
 
 
-app.listen(3000)
